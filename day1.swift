338fd1ddc598c906454f5a1e974c08d3396927f4
Antony Suba <lusianthony@gmail.com>
2023-09-01 10:11:24 +0300

    Day one file

---
+++ day1.swift
@@ -0,0 +1,4 @@
+let tempCelcius = 43.0
+let tempFahrenheit = ((tempCelcius * 9.0)/5.0) + 32.0
+print("Temperature in celcius is \(tempCelcius)")
+print("Temperature in Fahrenheit is \(tempFahrenheit)")
